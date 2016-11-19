b8be4656846592fffd758c8337a9607b381033d2
10195418 <yang.kai5@zte.com.cn>
2016-11-19 19:06:23 +0800

    cd

--- src/app/main/navbar/navbar.component.ts
+++ src/app/main/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'yk-navbar',
+  selector: 'px-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
@@ -10,10 +10,6 @@ export class NavbarComponent {
 
   @Output() collapse = new EventEmitter();
   isCollapsed:boolean = false;
-
-  source() {
-    window.open('http://www.github.com/njustyangkai/venus-frontend', '_blank');
-  }
 
   collapseSidebar(e:any) {
     this.isCollapsed = !this.isCollapsed;
