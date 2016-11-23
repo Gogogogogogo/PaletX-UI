7c7434be2fd0dca6f8d92df7dc15e67b89f70b7d
10195418 <yang.kai5@zte.com.cn>
2016-11-23 20:39:28 +0800

    css

--- src/app/css/button/css-button.component.ts
+++ src/app/css/button/css-button.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
 })
 
 export class CssButtonComponent {
-  isCollapse:boolean = true;
+  isCollapse1:boolean = true;
+  isCollapse2:boolean = true;
+  isCollapse3:boolean = true;
+  isCollapse4:boolean = true;
+  isCollapse5:boolean = true;
+  isCollapse6:boolean = true;
+  isCollapse7:boolean = true;
 }
