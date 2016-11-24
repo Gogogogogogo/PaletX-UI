2140a6760c9fb26fbc9e133cbc5f33976dab8d83
10195418 <yang.kai5@zte.com.cn>
2016-11-24 17:45:18 +0800

    cs

--- src/app/css/modal/css-modal.component.ts
+++ src/app/css/modal/css-modal.component.ts
@@ -8,15 +8,26 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class CssModalComponent {
 
   isCollapse1:boolean = true;
+  isCollapse2:boolean = true;
   
   @ViewChild('modal1_template') modal1_template:any;
   modal1:any;
+
+  @ViewChild('modal2_template') modal2_template:any;
+  modal2:any;
 
   constructor(private modalService:NgbModal) {
   }
 
   open1() {
     this.modal1 = this.modalService.open(this.modal1_template, {
+      backdrop: true,
+      keyboard: true
+    });
+  }
+
+  open2() {
+    this.modal2 = this.modalService.open(this.modal2_template, {
       backdrop: true,
       keyboard: true
     });
