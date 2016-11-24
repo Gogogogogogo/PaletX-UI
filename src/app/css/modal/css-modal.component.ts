40f9bb489a04f2c5c245226cfc4025d436a968c7
10195418 <yang.kai5@zte.com.cn>
2016-11-24 16:52:27 +0800

    css

--- src/app/css/modal/css-modal.component.ts
+++ src/app/css/modal/css-modal.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   templateUrl: './css-modal.component.html'
 })
 
 export class CssModalComponent {
+  @ViewChild('modal1_template') modal1_template:any;
+  modal1:any;
 
+  constructor(private modalService:NgbModal) {
+  }
+
+  open1() {
+    this.modal1 = this.modalService.open(this.modal1_template, {
+      backdrop: true,
+      keyboard: true
+    });
+  }
 }
