0758c967eb484cdb1c7a759aca9042ee1b748665
杨恺 <njustyangkai@126.com>
2016-11-28 18:22:15 +0800

    xs

--- src/app/css/table/css-table.component.ts
+++ src/app/css/table/css-table.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { datas } from './datas';
 
 @Component({
   templateUrl: './css-table.component.html'
 })
 
 export class CssTableComponent {
-
+  datas:any = datas;
 }
---
+++ src/app/css/table/datas.ts
@@ -0,0 +1,38 @@
+export const datas:any[] = [
+  {
+    username: 'user1',
+    role: 'Administrator',
+    state: '激活',
+    status: '在线'
+  },
+  {
+    username: 'user2',
+    role: 'Administrator',
+    state: '激活',
+    status: '在线'
+  },
+  {
+    username: 'user3',
+    role: 'Administrator',
+    state: '激活',
+    status: '在线'
+  },
+  {
+    username: 'user4',
+    role: 'Administrator',
+    state: '激活',
+    status: '在线'
+  },
+  {
+    username: 'user5',
+    role: 'Administrator',
+    state: '激活',
+    status: '在线'
+  },
+  {
+    username: 'user6',
+    role: 'Administrator',
+    state: '激活',
+    status: '在线'
+  }
+];
